3d9250b473ff219bcbe58d10ed8b03dd628f165c
rinerinegoaway <reginesorita01@gmail.com>
2024-12-10 14:41:09 +0800

    Add files via upload

---
+++ 045_Sorita_Telan_F1db/045_Sorita_Telan_F1db/FrmMain.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace _045_Sorita_Telan_F1db
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.grdResult = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbobrand = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grdResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKeyword.Location = new System.Drawing.Point(39, 15);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(187, 24);
+            this.txtKeyword.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(39, 45);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(504, 44);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // grdResult
+            // 
+            this.grdResult.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdResult.Location = new System.Drawing.Point(26, 158);
+            this.grdResult.Name = "grdResult";
+            this.grdResult.Size = new System.Drawing.Size(548, 269);
+            this.grdResult.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(39, 95);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(166, 50);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(211, 95);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(160, 50);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(377, 95);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(165, 50);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(360, 17);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(91, 20);
+            this.txtMin.TabIndex = 9;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(461, 17);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(81, 20);
+            this.txtMax.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(451, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "-";
+            // 
+            // cbobrand
+            // 
+            this.cbobrand.FormattingEnabled = true;
+            this.cbobrand.Items.AddRange(new object[] {
+            "Nokia",
+            "Samsung",
+            "Apple",
+            "Xiaomi",
+            "Realme",
+            "Redme",
+            "Asus",
+            "Oppo",
+            "Vivo",
+            "Huawei"});
+            this.cbobrand.Location = new System.Drawing.Point(232, 15);
+            this.cbobrand.Name = "cbobrand";
+            this.cbobrand.Size = new System.Drawing.Size(121, 21);
+            this.cbobrand.TabIndex = 13;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(598, 434);
+            this.Controls.Add(this.cbobrand);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.grdResult);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKeyword);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView grdResult;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbobrand;
+    }
+}
---
+++ 045_Sorita_Telan_F1db/045_Sorita_Telan_F1db/FrmMain.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _045_Sorita_Telan_F1db
+{
+    public partial class FrmMain : Form
+    {
+        string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Z:\\045_Sorita_Regine\\045_Sorita_Telan_F1db\\dbPhone.mdb";
+        OleDbConnection conn;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+
+            string keyword = txtKeyword.Text;
+            string minPriceText = txtMin.Text;
+            string maxPriceText = txtMax.Text;
+            string brand = cbobrand.Text;
+
+           
+
+            string query = "SELECT model_desc AS MODEL, price AS PRICE, brandid AS BRAND FROM model WHERE 1=1";
+
+            using (OleDbConnection conn = new OleDbConnection(connStr))
+            {
+                conn.Open();
+                OleDbCommand cmd = new OleDbCommand(query, conn);
+
+
+                if (!string.IsNullOrWhiteSpace(keyword))
+                {
+                    query += " AND model_desc LIKE @keyword";
+                    cmd.Parameters.AddWithValue("@keyword", "%" + keyword + "%");
+                }
+
+                if (decimal.TryParse(minPriceText, out decimal minPrice) && decimal.TryParse(maxPriceText, out decimal maxPrice))
+                {
+                    query += " AND price BETWEEN @minPrice AND @maxPrice";
+                    cmd.Parameters.AddWithValue("@minPrice", minPrice);
+                    cmd.Parameters.AddWithValue("@maxPrice", maxPrice);
+                }
+
+                if (!string.IsNullOrWhiteSpace(brand))
+                {
+                    query += " AND model.brandid = @brand";
+                    cmd.Parameters.AddWithValue("@brand", get_equivalent(brand));
+                }
+
+                cmd.CommandText = query;
+
+
+                DataTable dt = new DataTable();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                adapter.Fill(dt);
+                grdResult.DataSource = dt;
+
+            }
+        }
+
+        public int get_equivalent(string brandvalue)
+        {
+            int value = 0;
+
+            if (brandvalue.Equals("Nokia"))
+            {
+                value = 1;
+            }
+            else if (brandvalue.Equals("Samsung"))
+            {
+                value = 2;
+            }
+            else if (brandvalue.Equals("Apple"))
+            {
+                value = 3;
+            }
+            else if (brandvalue.Equals("Xiaomi"))
+            {
+                value = 4;
+            }
+            else if (brandvalue.Equals("Realme"))
+            {
+                value = 5;
+            }
+            else if (brandvalue.Equals("Redmi"))
+            {
+                value = 6;
+            }
+            else if (brandvalue.Equals("Asus"))
+            {
+                value = 7;
+            }
+            else if (brandvalue.Equals("Oppo"))
+            {
+                value = 8;
+            }
+            else if (brandvalue.Equals("Vivo"))
+            {
+                value = 9;
+            }
+            else if (brandvalue.Equals("Huawei"))
+            {
+                value = 10;
+            }
+
+            return value;
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            frmAdd add = new frmAdd();
+            add.ShowDialog();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            frmEdit edit = new frmEdit();
+            edit.ShowDialog();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            frmDelete delete = new frmDelete();
+            delete.ShowDialog();
+        }
+    }
+}
